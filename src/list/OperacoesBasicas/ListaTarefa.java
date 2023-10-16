151b718e9e950dc335ce5ad1000ba7b68c5f94c8
Levi MF <levimferreira@outlook.com>
2023-10-16 20:27:16 -0300

    resolvendo lista tarefa
    Signed-off-by: Levi MF <levimferreira@outlook.com>

---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,52 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescrcaoTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("total de elementos: " + listaTarefa.obterNumeroTotalTarefas());
+
+
+        listaTarefa.adicionarTarefa("tarefa 1");
+        listaTarefa.adicionarTarefa("tarefa 2");
+        listaTarefa.adicionarTarefa("tarefa 3");
+        System.out.println("total de elementos: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("tarefa 3");
+        System.out.println("total de elementos: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescrcaoTarefas();
+
+    }
+}
